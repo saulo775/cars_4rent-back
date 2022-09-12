ffa7cabd495bd1f7b2f5f6aeadac927220f7961f
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-09-11 22:30:14 -0300

    feat: upload files

---
+++ src/modules/cars/useCases/importCategories/ImportCategoriesController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+import { ImportCategoriesUseCase } from "./ImportCategoriesUseCase";
+
+class ImportCategoriesController {
+  constructor(private importCategoriesUseCase: ImportCategoriesUseCase) {}
+
+  handle(req: Request, res: Response): Response {
+    const { file } = req;
+    this.importCategoriesUseCase.execute(file);
+    return res.status(200).send();
+  }
+}
+
+export { ImportCategoriesController };
