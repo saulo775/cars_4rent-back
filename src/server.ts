3815902902ebfcbdd027ef77e29e66867ac39e13
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-09-17 12:20:06 -0300

    feat: criação do dockerfile

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use(router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.listen(5500, () => {
-  console.log("Server running on port 5500");
+app.listen(3333, () => {
+  console.log("Server running on port 3333");
 });
